d2be63a3cbba6aa012ca4fbfafb5a3cc093105ef
Geoffrey Howland <geoff@gmail.com>
2023-02-03 12:57:53 +0700

    Wrapping up the config and web app creation

--- sireus.go
+++ sireus.go
@@ -57,44 +57,88 @@ func GetActionsHtml(bot Bot) string {
 	return output
 }
 
-var action_path string = "config/action.json"
-var curve_path_format string = "config/curves/%s.json"
+type AppConfig struct {
+	WebPath         string `json:"web_path"`
+	ActionPath      string `json:"action_path"`
+	CurvePathFormat string `json:"curve_path_format"`
+}
 
-func main() {
-	actionData, err := os.ReadFile((action_path))
+var app_config_path = "config/config.json"
+
+func LoadConfig() AppConfig {
+	app_config_data, err := os.ReadFile((app_config_path))
 	Check(err)
 
-	var bot Bot
+	var app_config AppConfig
+	json.Unmarshal([]byte(app_config_data), &app_config)
 
-	json.Unmarshal([]byte(actionData), &bot)
+	return app_config
+}
 
-	curve_path := fmt.Sprintf(curve_path_format, bot.Actions[0].Considerations[0].CurveName)
-	curveData, err := os.ReadFile((curve_path))
+func LoadCurveData(path string) CurveData {
+	curveData, err := os.ReadFile((path))
 	Check(err)
 
 	var curve_data CurveData
 	json.Unmarshal([]byte(curveData), &curve_data)
 
-	fmt.Println(curve_data)
+	return curve_data
+}
 
+func CreateHandlebarsEngine(app_config AppConfig) *handlebars.Engine {
 	// Handlebars Engine for Fiber
-	engine := handlebars.New("./web", ".hbs")
+	engine := handlebars.New(app_config.WebPath, ".hbs")
+
+	// Reload the templates on each render, good for development
+	engine.Reload(true) // Optional. Default: false
+
+	//// Debug will print each template that is parsed, good for debugging
+	//engine.Debug(true) // Optional. Default: false
+
+	//// Layout defines the variable name that is used to yield templates within layouts
+	//engine.Layout("embed") // Optional. Default: "embed"
 
 	raymond.RegisterHelper("botinfo", func(bot Bot) string {
 		return bot.Name + "  Actions: " + string(len(bot.Actions))
 	})
 
+	return engine
+}
+
+func CreateWebApp(engine *handlebars.Engine) *fiber.App {
 	app := fiber.New(fiber.Config{
 		Views: engine,
 	})
 
+	return app
+}
+
+func main() {
+	app_config := LoadConfig()
+
+	actionData, err := os.ReadFile((app_config.ActionPath))
+	Check(err)
+
+	var bot Bot
+	json.Unmarshal([]byte(actionData), &bot)
+
+	curve_path := fmt.Sprintf(app_config.CurvePathFormat, bot.Actions[0].Considerations[0].CurveName)
+	curve_data := LoadCurveData(curve_path)
+
+	engine := CreateHandlebarsEngine(app_config)
+
+	app := CreateWebApp(engine)
+
 	app.Get("/", func(c *fiber.Ctx) error {
 		//return c.SendString(GetActionsHtml(bot))
 		return c.Render("index", fiber.Map{
-			"info": "Testing 123!",
-			"bot":  bot,
+			"info":       "Testing 123!",
+			"bot":        bot,
+			"title":      "Sireus",
+			"curve_data": curve_data,
 		}, "layouts/main_common")
 	})
 
 	app.Listen(":3000")
+
 }
