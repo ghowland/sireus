f11c97a1716fa3add3de75fd99636ff28cf7ebe7
Geoffrey Howland <geoff@gmail.com>
2023-02-04 13:15:27 +0700

    Added Toasts to get feedback on operations, and to notify when features arent implemented yet

---
+++ web_static/js/sireus_util.js
@@ -0,0 +1,23 @@
+function Toast(style, heading, text)
+{
+    if (style != "error" && style != "warning" && style != "info" && style != "success") style = "info";
+
+    $.toast({
+        text: text, // Text that is to be shown in the toast
+        heading: heading, // Optional heading to be shown on the toast
+        icon: style, // Type of toast icon
+        showHideTransition: 'fade', // fade, slide or plain
+        allowToastClose: true, // Boolean value true or false
+        hideAfter: 3000, // false to make it sticky or number representing the miliseconds as time after which toast needs to be hidden
+        stack: 5, // false if there should be only one toast at a time or a number representing the maximum number of toasts to be shown at a time
+        position: 'bottom-right', // bottom-left or bottom-right or bottom-center or top-left or top-right or top-center or mid-center or an object representing the left, right, top, bottom values
+
+        textAlign: 'left',  // Text alignment i.e. left, right or center
+        loader: true,  // Whether to show loader or not. True by default
+        loaderBg: '#9EC600',  // Background color of the toast loader
+        beforeShow: function () {}, // will be triggered before the toast is shown
+        afterShown: function () {}, // will be triggered after the toat has been shown
+        beforeHide: function () {}, // will be triggered before the toast gets hidden
+        afterHidden: function () {}  // will be triggered after the toast has been hidden
+    });
+}
