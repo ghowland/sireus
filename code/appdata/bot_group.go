875c611007c594fa7b96ffbc48ef97421fc8759f
Geoffrey Howland <geoff@gmail.com>
2023-02-05 00:58:24 +0700

    Good coverage on all the data structures needed to get the prototype working are set up. Next up is to make the initial JSON to populate this, and make pages to view the JSON. Then start running the queries and scoring the actions to run commands.

--- code/appdata/bot_group.go
+++ code/appdata/bot_group.go
@@ -1,5 +1,7 @@
 package appdata
 
+import "time"
+
 type ActionConsideration struct {
 	Name       string  `json:"name"`
 	Weight     float32 `json:"weight"`
@@ -8,7 +10,42 @@ type ActionConsideration struct {
 	RangeEnd   float32 `json:"range_end"`
 }
 
+type ActionCommandType int64
+
+const (
+	Bash ActionCommandType = iota
+	WebHttps
+	WebHttpInsecure
+	WebRPC
+)
+
+func (act ActionCommandType) String() string {
+	switch act {
+	case Bash:
+		return "Bash"
+	case WebHttps:
+		return "WebHttps"
+	case WebHttpInsecure:
+		return "WebHttpInsecure"
+	case WebRPC:
+		return "WebRPC"
+	}
+	return "Unknown"
+}
+
+type ActionCommandResult struct {
+	ActionName    string
+	ResultStatus  string
+	ResultContent string
+	HostExecOn    string
+}
+
 type ActionCommand struct {
+	Type           ActionCommandType `json:"type"`
+	Content        string            `json:"content"`
+	SuccessStatus  int               `json:"success_status"`
+	SuccessContent string            `json:"success_content"`
+	HostExecKey    string            `json:"host_exec_key"`
 }
 
 type Action struct {
@@ -16,27 +53,32 @@ type Action struct {
 	Info           string                `json:"info"`
 	Weight         float32               `json:"weight"`
 	WeightMin      float32               `json:"weight_min"`
+	Command        ActionCommand         `json:"command"`
 	Considerations []ActionConsideration `json:"considerations"`
-	Command        ActionCommand
+}
+
+type BotVariableValue struct {
+	Name  string
+	Value float32
+	Time  time.Time
 }
 
 type Bot struct {
 	Name    string   `json:"name"`
 	Info    string   `json:"info"`
 	Actions []Action `json:"actions"`
+
+	VariableValues []BotVariableValue
 }
 
 type BotQueryType int64
 
 const (
-	Undefined BotQueryType = iota
-	PrometheusQueryRange
+	PrometheusQueryRange BotQueryType = iota
 )
 
 func (bqt BotQueryType) String() string {
 	switch bqt {
-	case Undefined:
-		return "Undefined"
 	case PrometheusQueryRange:
 		return "PromQueryRange"
 	}
@@ -44,20 +86,88 @@ func (bqt BotQueryType) String() string {
 }
 
 type BotQuery struct {
-	Type BotQueryType `json:"query_type"`
-	Name string       `json:"name"`
-	Info string       `json:"info"`
+	QueryServer string `json:"query_server"`
+	Name        string `json:"name"`
+	Info        string `json:"info"`
+	Query       string `json:"query"`
+}
+
+type BotForwardSequenceState struct {
+	Name   string   `json:"name"`
+	Info   string   `json:"info"`
+	Labels []string `json:"labels"`
+}
+
+type BotExtractorQueryKey struct {
+	QueryName string `json:"query_name"`
+	Keys      string `json:"key"`
+}
+
+type BotLockTimer struct {
+	Name     string `json:"name"`
+	IsActive bool
+	Timeout  time.Time
+}
+
+type BotVariableType int64
+
+const (
+	Boolean BotVariableType = iota
+	Float
+)
+
+func (bvt BotVariableType) String() string {
+	switch bvt {
+	case Boolean:
+		return "Boolean"
+	case Float:
+		return "Float"
+	}
+	return "Unknown"
+}
+
+type BotVariable struct {
+	Type           BotVariableType `json:"type"`
+	Name           string          `json:"name"`
+	QueryName      string          `json:"query_name"`
+	QueryKey       string          `json:"query_key"`
+	BoolRangeStart float32         `json:"bool_range_start"`
+	BoolRangeEnd   float32         `json:"bool_range_end"`
+	BoolInvert     bool            `json:"bool_invert"`
 }
 
 type BotGroup struct {
-	Name    string   `json:"name"`
-	Info    string   `json:"info"`
-	Actions []Action `json:"actions"`
-	Bots    []Bot
+	Name             string                    `json:"name"`
+	Info             string                    `json:"info"`
+	Queries          []BotQuery                `json:"queries"`
+	BotExtractor     BotExtractorQueryKey      `json:"bot_extractor"`
+	States           []BotForwardSequenceState `json:"states"`
+	Actions          []Action                  `json:"actions"`
+	Variables        []BotVariable             `json:"variables"`
+	LockTimers       []BotLockTimer            `json:"lock_timers"`
+	BotTimeoutStale  time.Duration             `json:"bot_timeout_stale"`
+	BotTimeoutRemove time.Duration             `json:"bot_timeout_remove"`
+	Bots             []Bot
+
+	// Invalid = Isn't getting all the information.  Stale = Information out of data.  Removed = No data for too long, removing.
+	InvalidBots []string
+	StaleBots   []string
+	RemovedBots []string
+}
+
+type BotQueryServer struct {
+	Type         BotQueryType  `json:"type"`
+	Name         string        `json:"name"`
+	Info         string        `json:"info"`
+	Host         string        `json:"host"`
+	Port         int           `json:"port"`
+	Auth         string        `json:"auth"`
+	DataDuration time.Duration `json:"data_duration"`
 }
 
 type Site struct {
-	Name      string   `json:"name"`
-	Paths     []string `json:"paths"`
-	BotGroups []BotGroup
+	Name         string           `json:"name"`
+	Paths        []string         `json:"paths"`
+	QueryServers []BotQueryServer `json:"query_servers"`
+	BotGroups    []BotGroup
 }
