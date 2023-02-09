b5c8090ed8bdff11a321bd4f2c8557239c926274
Geoffrey Howland <geoff@gmail.com>
2023-02-09 15:30:21 +0700

    Updated docs...

--- code/data/site_data.go
+++ code/data/site_data.go
@@ -9,6 +9,6 @@ type (
 		BotGroupPaths []string      `json:"bot_group_paths"` // Paths to bot_group_name.json configs
 		QueryServers  []QueryServer `json:"query_servers"`   // List of QueryServers for making BotQuery requests
 		BotGroups     []BotGroup    // These configure and contain ephemeral Bots which perform the Action scoring in the active States
-		FreezeActions bool          // If true, no actions will be taken for this Site.  Allows control of all BotGroups.
+		FreezeActions bool          // If true, no actions will be taken for this Site.  Allows control of all BotGroups Action execution.
 	}
 )
