e5c633fe57ae825cf63c67a7778e848d87838c83
Geoffrey Howland <geoff@gmail.com>
2023-02-13 23:27:20 +0700

    Preparing for the demo and the prometheus exporter next

--- code/data/config_data.go
+++ code/data/config_data.go
@@ -12,7 +12,9 @@ type (
 		QueryFastDuration                 Duration `json:"query_fast_duration"`                  // Duration QueryFastInterval is maintained after the last user interaction
 		InteractiveSessionTimeout         Duration `json:"interactive_session_timeout"`          // Duration an InteractiveSession is kept until it is assumed finished, and can be purged
 		InteractiveDurationMinutesDefault int      `json:"interactive_duration_minutes_default"` // How many minutes we default to starting the interactive query to.  15 minutes is reasonable
-		ReloadTemplatesAlways             bool     `json:"reload_templates_always"`              // For development, if true this will always reload Handlebars files.  Only need to reestart the server to rebuild code.
+		PrometheusExportPort              int      `json:"prometheus_export_port"`               // Port used to listen for the Prometheus Exporter data we will put back into Prometheus.  For the main application, and the demo, if it is enabled
+		EnableDemo                        bool     `json:"enable_demo"`                          // If true, the demo will be enabled and will export additional metrics to Prometheus to make learning Sireus easier.  The demo shares the PrometheusExportPort for simplicity
+		ReloadTemplatesAlways             bool     `json:"reload_templates_always"`              // For development, if true this will always reload Handlebars files.  Only need to restart the server to rebuild code.
 		LogTemplateParsing                bool     `json:"log_template_parsing"`                 // For web development debugging, if true this will print out all the templates that are parsed.  It's not generally useful, but if you are having a problem with Handlebars template imports or related it can help
 	}
 )
--- go.mod
+++ go.mod
@@ -14,17 +14,26 @@ require (
 
 require (
 	github.com/andybalholm/brotli v1.0.4 // indirect
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/cespare/xxhash/v2 v2.1.2 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/klauspost/compress v1.15.15 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.17 // indirect
 	github.com/mattn/go-runewidth v0.0.14 // indirect
+	github.com/matttproud/golang_protobuf_extensions v1.0.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/prometheus/client_golang v1.14.0 // indirect
+	github.com/prometheus/client_model v0.3.0 // indirect
+	github.com/prometheus/common v0.37.0 // indirect
+	github.com/prometheus/procfs v0.8.0 // indirect
 	github.com/rivo/uniseg v0.4.3 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.44.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
 	golang.org/x/sys v0.4.0 // indirect
+	google.golang.org/protobuf v1.28.1 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
